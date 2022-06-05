2c409e60313b4a06d6bc8bb25987df3f05ddd0b7
maskey <pawan@actnepal.com.np>
2022-06-05 18:17:28 +0545

    login

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:ecommerce_app/constants/globalvarialble.dart';
+import 'package:ecommerce_app/features/auth/screens/auth_screen.dart';
+import 'package:ecommerce_app/homescreen.dart';
+import 'package:ecommerce_app/routes.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Ecommerce App',
+        theme: ThemeData(
+          scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+          colorScheme: const ColorScheme.light(
+            primary: GlobalVariables.secondaryColor,
+          ),
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            iconTheme: IconThemeData(color: Colors.black),
+          ),
+        ),
+        onGenerateRoute: (settings) => generateRoute(settings),
+        home: const AuthScreen());
+  }
+}
